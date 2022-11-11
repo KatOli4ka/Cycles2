f665f80da5f2c1e1891a308807c02f17e71647cb
KatOli4ka <katoli4ka87@mail.ru>
2022-11-11 19:35:08 +0500

    дз1-задача1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("ДЗ-1. Задание 1");
+int deposit=15000;
+int total=0;
+int i=0;
+while(total < 2_459_000){
+        total=total+deposit;i++;
+    System.out.println("Месяц " +i+", сумма накоплений равна "+total+" рублей");
+    }
+
+        System.out.println("ДЗ-1. Задание 2");
+
+
+
+    }
+}
